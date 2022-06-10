750a7762e15627419690270c3f7adc16a4eeb0eb
Davi N Lima <davi.n.lima99@gmail.com>
2022-06-10 10:11:35 -0300

    birthday-cake-candles

--- birthday-cake-candles/main.py
+++ birthday-cake-candles/main.py
@@ -9,18 +9,24 @@ import os
 #
 
 def birthday_cake_candles(candles):
-    return "Teste"
+    max_candle = 0
+    for candle in candles:
+        max_candle = candle if candle > max_candle else max_candle
+
+    return candles.count(max_candle)
 
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    #
+    # candles_count = int(input().strip())
+    #
+    # candles = list(map(int, input().rstrip().split()))
 
-    candles_count = int(input().strip())
-
-    candles = list(map(int, input().rstrip().split()))
-
+    candles = [3, 2, 1, 3]
     result = birthday_cake_candles(candles)
+    print(result)
 
-    fptr.write(str(result) + '\n')
+    # fptr.write(str(result) + '\n')
 
-    fptr.close()
+    # fptr.close()
